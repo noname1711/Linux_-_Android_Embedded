d9860ecdf494d9d63a35ed8b1799ac2ae4eb60c5
Hùng Lê <119946155+noname1711@users.noreply.github.com>
2025-04-06 11:49:25 +0700

    Delete General Knowledge directory

--- General Knowledge/General Knowledge/src/sqrt_lib.c
+++
@@ -1,6 +0,0 @@
-#include "sqrt_lib.h"
-#include <math.h>
-
-double my_sqrt(double num) {
-	return sqrt(num);
-}
--- General Knowledge/inc/sqrt_lib.h
+++
@@ -1,6 +0,0 @@
-#ifndef SQRT_LIB_H
-#define SQRT_LIB_H
-
-double my_sqrt(double num);
-
-#endif 
